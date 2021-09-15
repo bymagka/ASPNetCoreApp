f8140759f4f6c95859ac294d4014200f4c7beb30
bymagka <bymagka@bk.ru>
2021-09-15 23:44:48 +0300

    Добавление списка сотрудников с представлением. Добавление представления деталей по сотрудникам из списка.

---
+++ ASPNetCoreApp/Controllers/EmployeesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPNetCoreApp.Models;
+
+namespace ASPNetCoreApp.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private static readonly List<Employee> EmployeesList = new()
+        {
+            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Age = 27, BirthdayDate = DateTime.Now.AddYears(-27) },
+            new Employee { Id = 2, LastName = "Кучаев", FirstName = "Константин", Age = 31, BirthdayDate = DateTime.Now.AddYears(-31) },
+            new Employee { Id = 3, LastName = "Семёнов", FirstName = "Семён", Age = 18, BirthdayDate = DateTime.Now.AddYears(-18) },
+        };
+
+        public IActionResult Index()
+        {
+            return View(EmployeesList);
+        }
+
+        public IActionResult Details(int? id)
+        {
+            Employee Employee = EmployeesList.Select(x => x)
+                                             .Where(x => x.Id == id)
+                                             .First();
+
+            return View(Employee);
+        }
+    }
+}
