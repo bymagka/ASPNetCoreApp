866062a8c4001cf3d207ce451fe4969da7df0867
bymagka <bymagka@bk.ru>
2021-09-20 00:14:37 +0300

    добавление деталей

--- ASPNetCoreApp/Controllers/EmployeesController.cs
+++ ASPNetCoreApp/Controllers/EmployeesController.cs
@@ -4,17 +4,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ASPNetCoreApp.Models;
+using ASPNetCoreApp.Data;
 
 namespace ASPNetCoreApp.Controllers
 {
     public class EmployeesController : Controller
     {
-        private static readonly List<Employee> EmployeesList = new()
+        private readonly IEnumerable<Employee> EmployeesList;
+
+        public EmployeesController()
         {
-            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Age = 27, BirthdayDate = DateTime.Now.AddYears(-27) },
-            new Employee { Id = 2, LastName = "Кучаев", FirstName = "Константин", Age = 31, BirthdayDate = DateTime.Now.AddYears(-31) },
-            new Employee { Id = 3, LastName = "Семёнов", FirstName = "Семён", Age = 18, BirthdayDate = DateTime.Now.AddYears(-18) },
-        };
+            EmployeesList = TestData.EmployeesList;
+        }
 
         public IActionResult Index()
         {
@@ -24,6 +25,9 @@ namespace ASPNetCoreApp.Controllers
         public IActionResult Details(int? id)
         {
             Employee Employee = EmployeesList.Single(x => x.Id == id);
+
+            if (Employee is null) return NotFound();
+            
 
             return View(Employee);
         }
---
+++ ASPNetCoreApp/Data/TestData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPNetCoreApp.Models;
+
+namespace ASPNetCoreApp.Data
+{
+    public static class TestData
+    {
+        public static  List<Employee> EmployeesList { get; } = new()
+        {
+            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Age = 27, BirthdayDate = DateTime.Now.AddYears(-27) },
+            new Employee { Id = 2, LastName = "Кучаев", FirstName = "Константин", Age = 31, BirthdayDate = DateTime.Now.AddYears(-31) },
+            new Employee { Id = 3, LastName = "Семёнов", FirstName = "Семён", Age = 18, BirthdayDate = DateTime.Now.AddYears(-18) },
+        };
+    }
+}
