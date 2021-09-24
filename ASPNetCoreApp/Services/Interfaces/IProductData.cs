0ffc21f07e16ea9d61a4253d4682da1a6f58a770
bymagka <bymagka@bk.ru>
2021-09-24 23:50:50 +0300

    добавление сервисов и данных

---
+++ ASPNetCoreApp/Services/Interfaces/IProductData.cs
@@ -0,0 +1,12 @@
+﻿using ASPNetCoreApp.Domain.Entities;
+using System.Collections.Generic;
+
+namespace ASPNetCoreApp.Services.Interfaces
+{
+    public interface IProductData
+    {
+        IEnumerable<Brand> GetBrands();
+
+        IEnumerable<Section> GetSections();
+    }
+}
---
+++ ASPNetCoreApp/Services/ProductDataManagementService.cs
@@ -0,0 +1,20 @@
+﻿using ASPNetCoreApp.Domain.Entities;
+using ASPNetCoreApp.Services.Interfaces;
+using System.Collections.Generic;
+using ASPNetCoreApp.Data;
+
+namespace ASPNetCoreApp.Services
+{
+    public class ProductDataManagementService : IProductData
+    {
+        public IEnumerable<Brand> GetBrands()
+        {
+            return TestData.Brands;
+        }
+
+        public IEnumerable<Section> GetSections()
+        {
+            return TestData.Sections;
+        }
+    }
+}
