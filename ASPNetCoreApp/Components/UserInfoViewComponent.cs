83780f45272f16054d92b09488258bf6786cbcab
bymagka <bymagka@bk.ru>
2021-10-11 00:14:47 +0300

    визуальный компонент верхней панели

---
+++ ASPNetCoreApp/Components/UserInfoViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ASPNetCoreApp.Services.Interfaces;
+using System.Collections.Generic;
+using ASPNetCoreApp.ViewModels;
+using System.Linq;
+
+namespace ASPNetCoreApp.Components
+{
+    public class UserInfoViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke() => User.Identity?.IsAuthenticated == true ? View("UserInfo") : View();
+
+    }
+}
