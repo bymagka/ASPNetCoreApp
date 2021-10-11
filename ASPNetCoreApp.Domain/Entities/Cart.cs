395c5887f210509bf6a294ccc21bdfeaab22e28b
bymagka <bymagka@bk.ru>
2021-10-11 13:13:58 +0300

    Заготовка интерфейса сервиса корзины

---
+++ ASPNetCoreApp.Domain/Entities/Cart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+
+namespace ASPNetCoreApp.Domain.Entities
+{
+    class Cart
+    {
+        public ICollection<CartItem> CartItems => new List<CartItem>();
+
+        public int ItemsCount => CartItems?.Sum(it => it.Quantity) ?? 0;
+    }
+
+    public class CartItem
+    {
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ ASPNetCoreApp/Services/Interfaces/ICartService.cs
@@ -0,0 +1,21 @@
+﻿
+
+using ASPNetCoreApp.ViewModels;
+
+namespace ASPNetCoreApp.Services.Interfaces
+{
+    public interface ICartService
+    {
+        void Add(int id);
+
+        void Decrement(int id);
+
+        void Remove(int id);
+
+        void Clear();
+
+        CartViewModel GetViewModel(); 
+
+
+    }
+}
