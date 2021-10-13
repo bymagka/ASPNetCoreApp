95cc85ceb3696c4ecfbd7bc79dd91f8e44f7f293
bymagka <bymagka@bk.ru>
2021-10-13 23:12:07 +0300

    список товаров в админке

--- ASPNetCoreApp/Areas/Admin/Controllers/ProductsController.cs
+++ ASPNetCoreApp/Areas/Admin/Controllers/ProductsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ASPNetCoreApp.Infostructure.Mappers;
+using ASPNetCoreApp.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +11,17 @@ namespace ASPNetCoreApp.Areas.Admin.Controllers
     [Area("Admin")]
     public class ProductsController : Controller
     {
+        private IProductData productData;
+
+        public ProductsController(IProductData productData)
+        {
+            this.productData = productData;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            
+            return View(productData.GetProducts().ToView());
         }
     }
 }
