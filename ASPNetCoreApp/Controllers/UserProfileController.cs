26b34dca9ea9dd85b886b02581051f8820b5e76c
bymagka <bymagka@bk.ru>
2021-10-14 22:23:57 +0300

    контроллер информации пользователя

--- ASPNetCoreApp/Controllers/UserProfileController.cs
+++ ASPNetCoreApp/Controllers/UserProfileController.cs
@@ -1,16 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ASPNetCoreApp.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ASPNetCoreApp.Infostructure.Mappers;
 
 namespace ASPNetCoreApp.Controllers
 {
+    [Authorize]
     public class UserProfileController : Controller
     {
         public IActionResult Index()
         {
             return View();
+        }
+
+
+        public async Task<IActionResult> Orders([FromServices] IOrderService orderService)
+        {
+            var orders = await orderService.GetUserOrders(User.Identity!.Name);
+
+            return View(orders.ToUserView());
+
         }
     }
 }
