454fbb267d98713fac8fe7b6d5ea2cbcaa3d84e9
bymagka <bymagka@bk.ru>
2021-10-18 20:17:38 +0300

    подключение тестового апи

---
+++ UI/ASPNetCoreApp/Controllers/WebAPIController.cs
@@ -0,0 +1,24 @@
+﻿using ASPNetCoreApp.Interfaces.TestApi;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetCoreApp.Controllers
+{
+    public class WebAPIController : Controller
+    {
+        private readonly IValuesService valuesService;
+
+        public WebAPIController(IValuesService ValuesService)
+        {
+            valuesService = ValuesService;
+        }
+        public IActionResult Index()
+        {
+            var values = valuesService.GetAll();
+            return View(values);
+        }
+    }
+}
