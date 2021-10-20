0bcb227f575dee7cbe13403a5a8f5235aa705a28
bymagka <bymagka@bk.ru>
2021-10-20 22:17:23 +0300

    доработка базового клиента

--- Services/ASPNetCoreApp.WebAPI.Clients/Base/BaseClient.cs
+++ Services/ASPNetCoreApp.WebAPI.Clients/Base/BaseClient.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http.Json;
 
 namespace ASPNetCoreApp.WebAPI.Clients.Base
 {
@@ -17,6 +18,64 @@ namespace ASPNetCoreApp.WebAPI.Clients.Base
         {
             this.Client = Client;
             this.Adress = Adress;
+        }
+
+        protected T Get<T>(string url)
+        {
+            return GetAsync<T>(url).Result; 
+
+        }
+
+        protected async Task<T> GetAsync<T>(string url)
+        {
+            var response = await Client.GetAsync(url).ConfigureAwait(false);
+            return await response
+                .EnsureSuccessStatusCode()
+                .Content
+                .ReadFromJsonAsync<T>()
+                .ConfigureAwait(false);
+
+        }
+
+        protected HttpResponseMessage Delete<T>(string url)
+        {
+            return DeleteAsync<HttpResponseMessage>(url).Result;
+
+        }
+
+        protected async Task<HttpResponseMessage> DeleteAsync<T>(string url)
+        {
+            var response = await Client.DeleteAsync(url).ConfigureAwait(false);
+            return response
+                .EnsureSuccessStatusCode();
+
+        }
+        protected HttpResponseMessage Post<T>(string url,T item)
+        {
+            return PostAsync(url,item).Result;
+
+        }
+
+        protected async Task<HttpResponseMessage> PostAsync<T>(string url,T item)
+        {
+            var response = await Client.PostAsJsonAsync(url,item).ConfigureAwait(false);
+            return response
+                .EnsureSuccessStatusCode();
+
+        }
+
+        protected HttpResponseMessage Put<T>(string url, T item)
+        {
+            return PutAsync(url, item).Result;
+
+        }
+
+        protected async Task<HttpResponseMessage> PutAsync<T>(string url, T item)
+        {
+            var response = await Client.PutAsJsonAsync(url, item).ConfigureAwait(false);
+            return response
+                .EnsureSuccessStatusCode();
+
         }
     }
 }
