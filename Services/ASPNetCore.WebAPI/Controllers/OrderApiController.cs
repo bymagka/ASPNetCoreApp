747edb72f758c4b13bc9f9bf66236f6f770ce6ce
bymagka <bymagka@bk.ru>
2021-10-21 19:06:58 +0300

    апи контроллер заказов

---
+++ Services/ASPNetCore.WebAPI/Controllers/OrderApiController.cs
@@ -0,0 +1,50 @@
+﻿using ASPNetCoreApp.Interfaces.Services;
+using ASPNetCoreApp.Services.Mappers;
+using ASPNetCoreApp.Domain.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetCore.WebAPI.Controllers
+{
+    
+    [ApiController]
+    [Route("api/orders")]
+    public class OrderApiController : ControllerBase
+    {
+        private readonly IOrderService orderService;
+
+        public OrderApiController(IOrderService orderService)
+        {
+            this.orderService = orderService;
+        }
+
+        [HttpGet("user/{UserName}")]
+        public async Task<IActionResult> GetUserOrders(string UserName)
+        {
+            var orders = await orderService.GetUserOrders(UserName);
+            return Ok(orders.ToDTO());
+        } 
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOrderById(int id)
+        {
+            var order = await orderService.GetOrderById(id);
+
+            return order is null ? NotFound() : Ok(order.ToDTO());
+        }
+
+        [HttpPost("{UserName}")]
+        public async Task<IActionResult> CreateOrder(string UserName,[FromBody] CreateOrderDTO orderDTO)
+        {
+            var order = await orderService.CreateOrder(UserName, orderDTO.Items.ToCartView(), orderDTO.OrderModel);
+
+            return Ok(order.ToDTO());
+        }
+
+    }
+}
