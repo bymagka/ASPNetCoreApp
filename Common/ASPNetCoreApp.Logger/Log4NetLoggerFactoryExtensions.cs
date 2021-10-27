df9233eb437261cfda61776b68dcd8656c881b4c
bymagka <bymagka@bk.ru>
2021-10-27 22:30:24 +0300

    расширение для регистрации логгера

---
+++ Common/ASPNetCoreApp.Logger/Log4NetLoggerFactoryExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using System.Reflection;
+
+namespace ASPNetCoreApp.Logger
+{
+    public static class Log4NetLoggerFactoryExtensions
+    {
+        public static ILoggerFactory AddLog4Net(this ILoggerFactory Factory,string ConfigurationFile = "log4net.config")
+        {
+            Factory.AddProvider(new Log4NetLoggerProvider(CheckPath(ConfigurationFile)));
+            return Factory;
+        }
+
+        private static string CheckPath(string configurationFile)
+        {
+            if (configurationFile is not { Length: > 0 })
+                throw new ArgumentException("Can't find configuration file");
+
+            if (Path.IsPathRooted(configurationFile))
+                return configurationFile;
+
+            Assembly assembly = Assembly.GetEntryAssembly();
+            var path = Path.GetDirectoryName(assembly!.Location);
+
+            return Path.Combine(path!, configurationFile);
+
+        }
+    }
+}
