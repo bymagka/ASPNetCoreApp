ab8308f5ba68ba6403f7c7895e0cf76d71d6acef
bymagka <bymagka@bk.ru>
2021-10-30 23:21:26 +0300

    тесты контроллера home

---
+++ Tests/ASPNetCoreApp.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,72 @@
+﻿using ASPNetCoreApp.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assert = Xunit.Assert;
+
+namespace ASPNetCoreApp.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+
+        [TestMethod]
+        public void Index_Returns_View()
+        {
+            var controller = new HomeController();
+
+            var result = controller.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [TestMethod]
+        public void ContactUs_Returns_View()
+        {
+            var controller = new HomeController();
+
+            var result = controller.ContactUs();
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [TestMethod]
+        public void PageNotFound_Returns_View()
+        {
+            var controller = new HomeController();
+
+            var result = controller.PageNotFound();
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [TestMethod]
+        public void Blog_Returns_View()
+        {
+            var controller = new HomeController();
+
+            var result = controller.Blog();
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [TestMethod]
+        public void Blog_Single_Returns_View()
+        {
+            var controller = new HomeController();
+
+            var result = controller.BlogSingle();
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [TestMethod]
+        public void Checkout_Returns_View()
+        {
+            var controller = new HomeController();
+
+            var result = controller.Checkout();
+
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
