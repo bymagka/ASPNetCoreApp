9510c8573abd7066531e23000f2831608a0de1b0
bymagka <bymagka@bk.ru>
2021-11-04 21:40:46 +0300

    sitemap

---
+++ UI/ASPNetCoreApp/Controllers/API/SiteMapController.cs
@@ -0,0 +1,39 @@
+﻿using ASPNetCoreApp.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleMvcSitemap;
+    
+namespace ASPNetCoreApp.Controllers.AdfgPI
+{
+    public class SiteMapController : ControllerBase 
+    {
+        public IActionResult Index([FromServices] IProductData productData)
+        {
+            List<SitemapNode> nodes = new List<SitemapNode>
+            {
+                new SitemapNode(Url.Action("Index","Home")),
+                new SitemapNode(Url.Action("ContactUst","Home")),
+                new SitemapNode(Url.Action("Index","Catalog")),
+                new SitemapNode(Url.Action("Index","WebAPI")),
+            };
+
+            nodes.AddRange(productData.GetSections().Select(x => new SitemapNode(Url.Action("Index", "Catalog", new { SectionId = x.Id }))));
+
+            foreach(var brand in productData.GetBrands())
+            {
+                nodes.Add(new SitemapNode(Url.Action("Index", "Catalog", new { BrandId = brand.Id })));
+            }
+
+            foreach (var product in productData.GetProducts())
+            {
+                nodes.Add(new SitemapNode(Url.Action("Details", "Catalog", new { product.Id })));
+            }
+
+            return new SitemapProvider().CreateSitemap(new SitemapModel(nodes));
+        }
+    }
+}
