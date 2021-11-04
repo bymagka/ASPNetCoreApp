8ba449f822616d2e507c1a20055c3f3f63508cff
bymagka <bymagka@bk.ru>
2021-11-04 19:09:23 +0300

    основная логика таг хелпера

--- UI/ASPNetCoreApp/TagHelpers/ActiveRoute.cs
+++ UI/ASPNetCoreApp/TagHelpers/ActiveRoute.cs
@@ -26,9 +26,60 @@ namespace ASPNetCoreApp.TagHelpers
         [ViewContext, HtmlAttributeNotBound]
         public ViewContext ViewContext { get; set; }
 
+        [HtmlAttributeName("ws-is-active-route-active")]
+        public string ActiveCssClass { get; set; } = "active";
+
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
+
+            if (IsActive())
+            {
+                MakeActive(output);
+            }
+
             output.Attributes.RemoveAll(AttributeName);
+        }
+
+        private void MakeActive(TagHelperOutput output)
+        {
+            var class_attribute = output.Attributes.FirstOrDefault(attr => attr.Name == "class");
+
+            if(class_attribute is null)
+            {
+                output.Attributes.Add("class", ActiveCssClass);
+            }
+            else
+            {
+                if(class_attribute.Value?.ToString()?.Contains(ActiveCssClass) ?? false)
+                {
+                    return;
+                }
+
+                output.Attributes.SetAttribute("class", $"{class_attribute.Value} {ActiveCssClass}");
+            }
+        }
+
+        private bool IsActive()
+        {
+            var route_values = ViewContext.RouteData.Values;
+
+            var route_controller = route_values["controller"]?.ToString();
+
+            var route_action = route_values["action"]?.ToString();
+
+            if (Controller is { Length: > 0 } controller && !String.Equals(controller, route_controller))
+                return false;
+
+            if (Action is { Length: > 0 } action && !String.Equals(action, route_action))
+                return false;
+
+            foreach(var (key,value) in RouteValues)
+            {
+                if (!route_values.ContainsKey(key) || route_values[key]?.ToString() != value)
+                    return false;
+            }
+
+            return true;
         }
     }
 }
